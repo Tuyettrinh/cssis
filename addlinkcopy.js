d698df2a3afb2762a6a4f0af246853bde0534e34
Tuyettrinh <tuyettrinh1002@gmail.com>
2016-10-14 17:23:18 +0700

    Update addlinkcopy.js

--- addlinkcopy.js
+++ addlinkcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://congtyquangcaogoogletaiquan1.blogspot.com/'>http://congtyquangcaogoogletaiquan1.blogspot.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://tinhdaukhuechtantunhien.blogspot.com/'>https://tinhdaukhuechtantunhien.blogspot.com/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
